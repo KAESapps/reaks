0f3587bf868770d08bc96c16510050c3d3ca5ad1
Sylvain Vuilliot <sylvain@kaes.fr>
2017-10-03 11:11:48 +0200

    add valueAttr

---
+++ valueAttr.js
@@ -0,0 +1,8 @@
+const { autorun } = require('reactivedb/obs')
+
+module.exports = getValue => domNode =>
+  autorun(() => {
+    const value = getValue()
+    const domValue = domNode.value
+    if (domValue !== value) domNode.value = value // prevent setting the value if not necessary to prevent cursor jumping
+  })
