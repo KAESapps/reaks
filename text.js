f15e01221016b997ceecacad7b643d430a551a09
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2017-10-16 16:52:24 +0200

    text only display string values

--- text.js
+++ text.js
@@ -1,5 +1,12 @@
 const { autorun } = require('reactivedb/obs')
-const staticText = str => domNode => {
+const isString = require('lodash/isString')
+
+const staticText = v => domNode => {
+  let str = v
+  if (!isString(v)) {
+    str = ""
+    console.warn("reaks/text value is not a string, using empty string", v, domNode)
+  }
   const textNode = document.createTextNode(str)
   domNode.appendChild(textNode)
   return () => domNode.removeChild(textNode)
@@ -8,7 +15,11 @@ const dynamicText = getValue => domNode => {
   let textNode
   const unobserve = autorun(() => {
     const v = getValue()
-    const str = typeof v === 'string' ? v : JSON.stringify(v) || ''
+    let str = v
+    if (!isString(str)) {
+      str = ""
+      console.warn("reaks/text value is not a string, using empty string", v, domNode)
+    }
     if (textNode) {
       textNode.nodeValue = str
     } else {
