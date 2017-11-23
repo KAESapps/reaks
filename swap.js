8bece19e59ebaaf6afa95a366ee0d3857a9144c9
Sylvain Vuilliot <sylvain@kaes.fr>
2017-11-23 16:03:54 +0100

    swap: ne pas démonter le cmp à chaque pulse si c'est le même

--- swap.js
+++ swap.js
@@ -1,17 +1,22 @@
 const { autorun } = require("kobs")
 
+//
 module.exports = function(getCmp) {
+  let currentCmp
   return parentNode => {
     let unmount
     const unobserve = autorun(function() {
-      unmount && unmount()
       const cmp = getCmp()
+      if (cmp === currentCmp) return
+      currentCmp = cmp
+      unmount && unmount()
       unmount = cmp && cmp(parentNode)
     }, "reaks/swap")
 
     return () => {
       unobserve()
       unmount && unmount()
+      currentCmp = null // est-ce nécessaire ?
     }
   }
 }
